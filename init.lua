c899774ac087124914369c2dee50e2d85d415c8a
Foz <fozolo@gmail.com>
2016-04-02 17:24:01 -0400

    Improve code formatting

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 -- randomize player position within the spawn plot
 local function go_spawn(player)
 	if core.setting_get('static_spawnpoint') then
-	  local pos = core.setting_get_pos('static_spawnpoint')
-	  if core.setting_get('variable_spawnpoint') then
-		  local range = tonumber(core.setting_get('variable_spawnpoint'))
-		  if range > 0 then
-			  pos.x = pos.x + math.random(-(range), range)
-			  pos.z = pos.z + math.random(-(range), range)
-			  core.log("action", '[Spawn Plus] Moving player to pos '..pos.x..', '..pos.y)
-		  end
-	  end
-	  player:setpos( pos )
-  end
+		local pos = core.setting_get_pos('static_spawnpoint')
+		if core.setting_get('variable_spawnpoint') then
+			local range = tonumber(core.setting_get('variable_spawnpoint'))
+			if range > 0 then
+				pos.x = pos.x + math.random(-(range), range)
+				pos.z = pos.z + math.random(-(range), range)
+				core.log("action", '[Spawn Plus] Moving player to pos '..pos.x..', '..pos.y)
+			end
+		end
+		player:setpos( pos )
+	end
 end
 
 core.register_chatcommand('spawn', {
@@ -21,8 +21,10 @@ core.register_chatcommand('spawn', {
 	},
 	func = function(name)
 		local player = core.get_player_by_name(name)
-		core.sound_play("teleport",
-				{to_player=player:get_player_name(), gain = 0.1})
+		core.sound_play("teleport", {
+			to_player=player:get_player_name(),
+			gain = 0.1
+		})
 		go_spawn(player)
 	end
 })
@@ -35,8 +37,10 @@ core.register_chatcommand('arena', {
 	},
 	func = function(name)
 		local player = core.get_player_by_name(name)
-		core.sound_play("teleport",
-				{to_player=player:get_player_name(), gain = 0.1})
+		core.sound_play( "teleport", {
+			to_player=player:get_player_name(),
+			gain = 0.1
+		})
 		player:setpos( { x=595, y=6, z=405 } )
 	end
 })
@@ -52,14 +56,17 @@ local function find_free(pos)
 end
 
 local INTERVAL=3
+local LIMIT=4500
 local function check_pos_range()
 	for _,p in ipairs(core.get_connected_players()) do
-		local name = p:get_player_name()
 		local pos = p:getpos()
 		-- map limit
-		if math.abs(pos.x) > 4500 or math.abs(pos.z) > 4500 then
-			core.chat_send_player(p:get_player_name(), 'Sorry, but you should not go further than 4500 meters away.')
-      go_spawn(p)
+		if math.abs(pos.x) > LIMIT or math.abs(pos.z) > LIMIT then
+			core.chat_send_player(
+				p:get_player_name(),
+				'Sorry, but you should not go beyond '..tostring(LIMIT)..'.'
+			)
+			go_spawn(p)
 		end
 	end
 	core.after(INTERVAL, check_pos_range)
